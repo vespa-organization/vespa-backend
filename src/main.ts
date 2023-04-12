b3c6215836b4b72d3bed42b0dc07daf049459ea6
Benjamin Mecanović <benjamin.mecanovic@rimac-technology.com>
2023-04-12 12:47:36 +0200

    feat: add port via env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  const port = process.env.PORT || 3000
+  await app.listen(port)
+  console.log(`Application is running on: ${await app.getUrl()}`)
+  console.log(`GraphQL Playground: ${await app.getUrl()}/graphql`)
 }
-bootstrap();
+
+bootstrap()
